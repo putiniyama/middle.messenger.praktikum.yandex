c3cefba86fa1df200eb3a340bb7b079334b3e8a6
putiniyama <85397886+putiniyama@users.noreply.github.com>
2022-11-06 01:11:25 +0200

    rout + 3000 port

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.use(express.static('dist'))
+const express = require('express');
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const distDir = `${__dirname}/dist/`;
+app.use(express.static(distDir));
+
+app.get('/*', function (req, res) {
+  res.sendFile(`${distDir}/index.html`);
+});
+
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+});
